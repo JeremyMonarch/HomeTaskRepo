a4d3211b7f8ea565d658288c5306359c7833e29b
Mykyta Zakharov <mykytazakh@gmail.com>
2023-01-29 16:14:14 +0200

    Update

--- Preparation/Preparation/Program.cs
+++ Preparation/Preparation/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Preperation
 {
@@ -12,28 +13,12 @@ namespace Preperation
             string input = Console.ReadLine();
             int m = 0;
             int n = 0;
-            var split = input.Split(' ');
 
-            while(string.IsNullOrEmpty(input))
+            while(!IsValidInput(input, out m, out n))
             {
-                Console.WriteLine("Wrong insert! Please, try again");
+                Console.WriteLine("Wrong input!");
                 input = Console.ReadLine();
             }
-
-            while(!int.TryParse(split[0], out m))
-            {
-                Console.WriteLine("Wrong insert! Please, try again");
-                input = Console.ReadLine();
-                split = input.Split(' ');
-            }
-
-            while(!int.TryParse(split[1], out n))
-            {
-                Console.WriteLine("Wrong insert! Please, try again");
-                input = Console.ReadLine();
-                split = input.Split(' ');
-            }
-
             int[,] matrix = new int[m, n];
             string direction = "right";
             int matrixSize = m * n;
@@ -43,25 +28,25 @@ namespace Preperation
 
             for(int i = 1; i <= matrixSize; i++)
             {
-                if((direction == "right") && (y > n - 1 || x > m - 1 || matrix[x, y] != 0))
+                if(direction == "right" && (y > n - 1 || x > m - 1 || matrix[x, y] != 0))
                 {
                     direction = "down";
                     y--;
                     x++;
                 }
-                if((direction == "down") && (x > m - 1 || y > n - 1 || matrix[x, y] != 0))
+                if(direction == "down" && (x > m - 1 || y > n - 1 || matrix[x, y] != 0))
                 {
                     direction = "left";
                     x--;
                     y--;
                 }
-                if((direction == "left") && (y < 0 || x < 0 || matrix[x, y] != 0))
+                if(direction == "left" && (y < 0 || x < 0 || matrix[x, y] != 0))
                 {
                     direction = "up";
                     y++;
                     x--;
                 }
-                if((direction == "up") && (x < 0 || y < 0 || matrix[x, y] != 0))
+                if(direction == "up" && (x < 0 || y < 0 || matrix[x, y] != 0))
                 {
                     direction = "right";
                     x++;
@@ -92,7 +77,7 @@ namespace Preperation
                 for(int j = 0; j < n & j < m + (n - m); j++) //y
                 {
                     
-                    Console.Write("{0, 3}", matrix[i, j]);
+                    Console.Write("{0, 4}", matrix[i, j]);
                 }
                 Console.WriteLine();
             }
@@ -102,6 +87,18 @@ namespace Preperation
         {
             Console.WriteLine("Spiral array: ");
             Console.Write("\nInput two numbers (one by one with the space. E.g.: (5 5)) for create spiral array: ");
+        }
+
+        static bool IsValidInput(string input, out int m, out int n)
+        {
+            n = 0;
+            m = 0;
+            if(string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+            var split = input.Split(' ');
+            return int.TryParse(split[0], out m) && int.TryParse(split[1], out n);
         }
     }
 }
