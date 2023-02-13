e17b0e86bac431fe70eefb11fa8d15228622181e
Mykyta Zakharov <98880106+JeremyMonarch@users.noreply.github.com>
2023-02-13 22:06:40 +0200

    Try #3

--- Preparation/MyStack/MyStackInter.cs
+++ Preparation/MyStack/MyStackInter.cs
@@ -4,21 +4,60 @@ namespace MyStack
 {
     public class MyStack
     {
-        public int ;
+        public static readonly int maxsize = 10;
+        public int topstack;
+        public int[] arrayint = new int[maxsize];
 
-        public void Add(int add)
+        public MyStack()
         {
-            
+            topstack = 0;
         }
 
-        public void SeeLastInter(int[] intarray)
+        public bool Add(int add)
         {
-            
+            if (topstack >= maxsize)
+            {
+                Console.WriteLine("Stack Overfllow");
+                return false;
+            }
+            else
+            {
+                arrayint[++topstack] = add;
+                return true;
+            }
         }
 
-        public void Delete()
+        public void SeeLastInter()
         {
-            
+            if (topstack < 0)
+            {
+                Console.WriteLine("Stack is null");
+            }
+            Console.WriteLine($"Top element is  {topstack}");
+        }
+
+        public int Delete()
+        {
+            if (topstack < 0)
+            {
+                Console.WriteLine("Stack is null");
+            }
+
+            int value = arrayint[topstack--];
+            return value;
+        }
+
+        public void PrintStack()
+        {
+            if (topstack < 0)
+            {
+                Console.WriteLine("Stack is null");
+            }
+            Console.WriteLine("Your stack is ");
+            for (int i = topstack; i > 0; i--)
+            {
+                Console.WriteLine(arrayint[i]);
+            }
         }
     }
 }
