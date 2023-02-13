b13fb6782c4b7f9e68630d1fd9bdefd3a7137f3c
Mykyta Zakharov <98880106+JeremyMonarch@users.noreply.github.com>
2023-02-13 21:03:00 +0200

    Try #2

---
+++ Preparation/MyStack/MyStackInter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+
+namespace MyStack
+{
+    public class MyStack
+    {
+        public int ;
+
+        public void Add(int add)
+        {
+            
+        }
+
+        public void SeeLastInter(int[] intarray)
+        {
+            
+        }
+
+        public void Delete()
+        {
+            
+        }
+    }
+}
