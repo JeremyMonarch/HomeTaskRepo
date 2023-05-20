6bf4ea06dcb15027e93f3ba7ebcccda8821bf34d
Mykyta Zakharov <mykytazakh@gmail.com>
2023-05-20 17:04:25 +0300

    Unit tests
    Fix name and some test funcs

--- Preparation/TestProject1/MyStackInterTest.cs
+++ Preparation/TestProject1/MyStackInterTest.cs
@@ -1,4 +1,7 @@
 using MyStack;
+using System;
+using System.Data;
+using System.Linq;
 using Xunit;
 
 namespace Preperation.Tests
@@ -6,36 +9,56 @@ namespace Preperation.Tests
     public class MyStackInterTest
     {
         [Fact]
-        public void PushTest()
+        public void Push_AddsOneElementToTheCollection()
         {
             MyStack<int> myStack = new MyStack<int>();
 
             myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            myStack.Push(4);
 
-            Assert.Equal(1, myStack.Peek());
+            Assert.Equal(4, myStack.Count());
+            Assert.Equal(4, myStack.Last());
         }
         [Fact]
-        public void PeekTest()
+        public void Peek_ReturnsLastPushedElementsInTheCollectionTest()
         {
             MyStack<int> myStack = new MyStack<int>();
+            myStack.Push(1);
+            myStack.Push(20);
+            myStack.Push(30);
+
+            int result = myStack.Peek();
+
+            Assert.Equal(30, result);
+            Assert.Equal(3, myStack.Count());
+        }
+        [Fact]
+        public void Pop_RemoveTopElementInTheCollectionTest()
+        {
+            MyStack<int> myStack = new MyStack<int>();
+            
             myStack.Push(1);
             myStack.Push(2);
             myStack.Push(3);
 
-            int result = myStack.Peek();
+            int result = myStack.Pop();
 
             Assert.Equal(3, result);
+            Assert.Equal(2, myStack.Count());
         }
         [Fact]
-        public void PopTest()
+        public void Pop_TryingRemoveElementInEmptyCollection()
         {
             MyStack<int> myStack = new MyStack<int>();
+
             myStack.Push(1);
-            myStack.Push(2);
 
             int result = myStack.Pop();
 
-            Assert.Equal(2, result);
+            Assert.Equal(1, result);
+            Assert.Throws<InvalidOperationException>(() => myStack.Pop());
         }
         [Fact]
         public void IsEmptyTrueTest()
