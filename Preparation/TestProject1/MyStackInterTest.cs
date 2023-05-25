41922fb3d25d515a24beb73ea9b78eedd2283167
Mykyta Zakharov <mykytazakh@gmail.com>
2023-05-25 13:52:44 +0300

    Update Test File

--- Preparation/TestProject1/MyStackInterTest.cs
+++ Preparation/TestProject1/MyStackInterTest.cs
@@ -14,12 +14,22 @@ namespace Preperation.Tests
             MyStack<int> myStack = new MyStack<int>();
 
             myStack.Push(1);
-            myStack.Push(2);
-            myStack.Push(3);
-            myStack.Push(4);
 
-            Assert.Equal(4, myStack.Count());
-            Assert.Equal(4, myStack.Last());
+            Assert.Single(myStack);
+        }
+        [Fact]
+        public void Push_CountIncreasesByOneAfterPush()
+        {
+            MyStack<int> myStack = new MyStack<int>();
+            
+            myStack.Push(1);
+            
+            int countBeforePush = myStack.Count();
+            myStack.Push(2);
+            int countAfterPush = myStack.Count();
+
+            Assert.Equal(2, myStack.Count());
+            Assert.Equal(countBeforePush + 1, countAfterPush);
         }
         [Fact]
         public void Peek_ReturnsLastPushedElementsInTheCollectionTest()
@@ -27,12 +37,31 @@ namespace Preperation.Tests
             MyStack<int> myStack = new MyStack<int>();
             myStack.Push(1);
             myStack.Push(20);
-            myStack.Push(30);
 
             int result = myStack.Peek();
 
-            Assert.Equal(30, result);
-            Assert.Equal(3, myStack.Count());
+            Assert.Equal(20, result);
+            Assert.Equal(2, myStack.Count());
+        }
+        [Fact]
+        public void Peek_ThrowsInvalidOperationException_WhenStackIsEmpty()
+        {
+            MyStack<int> myStack = new MyStack<int>();
+
+            Assert.Throws<InvalidOperationException>(() => myStack.Peek());
+        }
+        [Fact]
+        public void Peek_CountUnchangedDuringPeek()
+        {
+            MyStack<int> myStack = new MyStack<int>();
+            myStack.Push(1);
+            myStack.Push(2);
+
+            int countBeforePeek = myStack.Count();
+            myStack.Peek();
+            int countAfterPeek = myStack.Count();
+
+            Assert.Equal(countBeforePeek, countAfterPeek);
         }
         [Fact]
         public void Pop_RemoveTopElementInTheCollectionTest()
@@ -49,34 +78,27 @@ namespace Preperation.Tests
             Assert.Equal(2, myStack.Count());
         }
         [Fact]
-        public void Pop_TryingRemoveElementInEmptyCollection()
+        public void Pop_CountDecreasesByOneElementAfterPop()
         {
             MyStack<int> myStack = new MyStack<int>();
 
             myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
 
-            int result = myStack.Pop();
+            int countBeforePop = myStack.Count();
+            int popValue = myStack.Pop();
+            int countAfterPop = myStack.Count();
 
-            Assert.Equal(1, result);
-            Assert.Throws<InvalidOperationException>(() => myStack.Pop());
+            Assert.Equal(3, popValue);
+            Assert.Equal(countBeforePop - 1, countAfterPop);
         }
         [Fact]
-        public void IsEmptyTrueTest()
+        public void Pop_ThrowsInvalidOperationException_WhenStackIsEmpty()
         {
             MyStack<int> myStack = new MyStack<int>();
 
-            bool isEmpty = myStack.IsEmpty();
-
-            Assert.True(isEmpty);
-        }
-        public void IsEmptyFalseTest()
-        {
-            MyStack<int> myStack = new MyStack<int>();
-            myStack.Push(5);
-
-            bool isEmpty = myStack.IsEmpty();
-
-            Assert.False(isEmpty);
+            Assert.Throws<InvalidOperationException>(() => myStack.Pop());
         }
     }
 }
